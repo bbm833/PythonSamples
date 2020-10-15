e767a1c2a37215d6c49a2dcc30e4c6ca2bd80636
Sadettin Demiray <sadettin.demiray@partner.bmw.de>
2020-10-16 00:10:16 +0200

    Add file read/write

--- task2.py
+++ task2.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import pickle
+
+
 class PhoneBook:
-    def __init__(self):
+    def __init__(self, phone_book_file_name):
         self.phone_book = {}
+        self.phone_book_file_name = phone_book_file_name
 
     def add_contact(self, name, phone_number):
         if name not in self.phone_book:
@@ -11,7 +15,7 @@ class PhoneBook:
 
         return False
 
-    def __function_generator__(function):
+    def __function_generator(function):
         def phone_book_operation(self, *args):
             name = args[0]
             if name in self.phone_book:
@@ -20,17 +24,17 @@ class PhoneBook:
                 return None
         return phone_book_operation
 
-    @__function_generator__
+    @__function_generator
     def update_contact(self, name, phone_number):
         self.phone_book[name] = phone_number
         return True
 
-    @__function_generator__
+    @__function_generator
     def delete_contact(self, name):
         del self.phone_book[name]
         return True
 
-    @__function_generator__
+    @__function_generator
     def find_contact(self, name):
         return self.phone_book[name]
 
@@ -41,11 +45,32 @@ class PhoneBook:
             print(f"{name}\t\t\t\t{phone_number}")
         print("----------------------------------------------------------------------------\n")
 
+    def read_phone_book_file(self):
+        is_file_read = True
+        try:
+            with open(self.phone_book_file_name, 'rb') as phone_book_file:
+                self.phone_book = pickle.load(phone_book_file)
+        except (IOError, OSError, pickle.PickleError, pickle.UnpicklingError):
+            is_file_read = False
+        return is_file_read
+
+    def write_phone_book_file(self):
+        is_file_written = True
+        try:
+            with open(self.phone_book_file_name, 'wb') as phone_book_file:
+                pickle.dump(self.phone_book, phone_book_file)
+        except (IOError, OSError, pickle.PickleError, pickle.UnpicklingError):
+            is_file_written = False
+        return is_file_written
+
 
 class PhoneBookApplication:
 
-    def __init__(self):
-        self.phone_book = PhoneBook()
+    def __init__(self, phone_book_file_name):
+        self.phone_book = PhoneBook(phone_book_file_name)
+        is_file_read = self.phone_book .read_phone_book_file()
+        if not is_file_read:
+            print(f"{phone_book_file_name} is not a valid phone book file")
 
     @staticmethod
     def print_menu():
@@ -64,33 +89,38 @@ class PhoneBookApplication:
 ############################################################################""")
 
     def add_contact(self):
-        name = self.__get_name_from_console__()
-        phone_number = self.__get_number_from_console__()
+        name = self.__get_name_from_console()
+        phone_number = self.__get_number_from_console()
 
-        is_added = self.phone_book.add_contact(name, phone_number)
-        if is_added:
+        is_contact_added = self.phone_book.add_contact(name, phone_number)
+        if is_contact_added:
             print(f"Contact '{name}' is added to the phone book")
         else:
-            is_confirmed = self.__is_operation_confirmed__(f"{name} is already in the phone book. "
+            is_confirmed = self.__is_operation_confirmed(f"{name} is already in the phone book. "
                                                            "Do you want to update the number [Y/n]: ")
             if is_confirmed:
                 self.update_contact(name, phone_number)
+                is_contact_added = True
+
+        if is_contact_added:
+            self.__write_phone_book_file()
 
     def update_contact(self, name=None, phone_number=None):
         if name is None:
-            name = self.__get_name_from_console__()
+            name = self.__get_name_from_console()
 
         if phone_number is None:
-            phone_number = self.__get_number_from_console__()
+            phone_number = self.__get_number_from_console()
 
         is_updated = self.phone_book.update_contact(name, phone_number)
         if is_updated:
+            self.__write_phone_book_file()
             print(f"Phone number of '{name}' is updated as '{phone_number}'")
         else:
             print(f"'{name}' is not in the phone book.")
 
     def find_contact(self):
-        name = self.__get_name_from_console__()
+        name = self.__get_name_from_console()
         phone_number = self.phone_book.find_contact(name)
 
         if phone_number:
@@ -99,12 +129,13 @@ class PhoneBookApplication:
             print(f"'{name}' is not in phone book.")
 
     def delete_contact(self):
-        name = self.__get_name_from_console__()
-        is_confirmed = self.__is_operation_confirmed__(f"Do you want to delete contact '{name}' [Y/n]: ")
+        name = self.__get_name_from_console()
+        is_confirmed = self.__is_operation_confirmed(f"Do you want to delete contact '{name}' [Y/n]: ")
 
         if is_confirmed:
             is_deleted = self.phone_book.delete_contact(name)
             if is_deleted:
+                self.__write_phone_book_file()
                 print(f"Contact '{name}' is deleted.")
             else:
                 print(f"'{name}' is not in the phone book.")
@@ -112,16 +143,21 @@ class PhoneBookApplication:
     def print_phone_book(self):
         self.phone_book.print()
 
+    def __write_phone_book_file(self):
+        is_file_written = self.phone_book.write_phone_book_file()
+        if not is_file_written:
+            print("Phone book file could not be updated")
+
     @staticmethod
-    def __get_name_from_console__():
+    def __get_name_from_console():
         return str(input("Enter the name of the contact: "))
 
     @staticmethod
-    def __get_number_from_console__():
+    def __get_number_from_console():
         return str(input("Enter the phone number of the contact: "))
 
     @staticmethod
-    def __is_operation_confirmed__(message):
+    def __is_operation_confirmed(message):
         while True:
             answer = str(input(message))
             if answer == "Y" or answer == "y":
@@ -133,7 +169,7 @@ class PhoneBookApplication:
 
 
 if __name__ == "__main__":
-    phone_book_application = PhoneBookApplication()
+    phone_book_application = PhoneBookApplication("MyPhoneBook.pb")
     while True:
         phone_book_application.print_menu()
         selection = str(input("Please make a selection [1-6] "))
